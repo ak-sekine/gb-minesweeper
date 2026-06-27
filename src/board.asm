8c5c07f7e8d5c1a368d2ee2fb78d04089e32b1f8
Akihiro Sekine <akihiro3729@outlook.jp>
2026-06-27 18:18:50 +0900

    数字生成処理実装。

--- src/board.asm
+++ src/board.asm
@@ -17,7 +17,13 @@ wMinesPlaced::
 IF DEBUG_SHOW_MINES
 wDebugMinesDrawPending::
     ds 1
+wDebugDrawRow::
+    ds 1
 ENDC
+wBoardGenX::
+    ds 1
+wBoardGenY::
+    ds 1
 
 SECTION "Board", ROM0
 
@@ -26,6 +32,7 @@ Board_Init::
     ld [wMinesPlaced], a
 IF DEBUG_SHOW_MINES
     ld [wDebugMinesDrawPending], a
+    ld [wDebugDrawRow], a
 ENDC
     ld hl, wBoard
     ld b, BOARD_CELL_COUNT
@@ -46,9 +53,13 @@ IF DEBUG_SHOW_MINES
     ld a, [wDebugMinesDrawPending]
     and a
     ret z
-    call Board_DebugDrawMines
+    call Board_DebugDrawNextRow
+    ld a, [wDebugDrawRow]
+    cp BOARD_HEIGHT
+    ret c
     xor a
     ld [wDebugMinesDrawPending], a
+    ld [wDebugDrawRow], a
 ENDC
     ret
 
@@ -78,11 +89,129 @@ Board_PlaceMinesIfNeeded::
     dec d
     jr nz, .placeNext
 
+    call Board_GenerateNumbers
+
     ld a, 1
     ld [wMinesPlaced], a
 IF DEBUG_SHOW_MINES
     ld [wDebugMinesDrawPending], a
+    xor a
+    ld [wDebugDrawRow], a
 ENDC
+    ret
+
+Board_GenerateNumbers:
+    ld hl, wBoard
+    ld b, BOARD_CELL_COUNT
+.clearNumbers:
+    ld a, [hl]
+    and $F0
+    ld [hli], a
+    dec b
+    jr nz, .clearNumbers
+
+    ld hl, wBoard
+    xor a
+    ld [wBoardGenY], a
+.row:
+    xor a
+    ld [wBoardGenX], a
+.column:
+    bit CELL_MINE_BIT, [hl]
+    jr z, .nextCell
+    push hl
+    call Board_IncrementMineNeighbors
+    pop hl
+.nextCell:
+    inc hl
+    ld a, [wBoardGenX]
+    inc a
+    ld [wBoardGenX], a
+    cp BOARD_WIDTH
+    jr c, .column
+    ld a, [wBoardGenY]
+    inc a
+    ld [wBoardGenY], a
+    cp BOARD_HEIGHT
+    jr c, .row
+    ret
+
+Board_IncrementMineNeighbors:
+    ld a, [wBoardGenY]
+    and a
+    jr z, .sameRow
+    dec a
+    call Board_IncrementNeighborRow
+
+.sameRow:
+    ld a, [wBoardGenY]
+    call Board_IncrementSideNeighbors
+
+    ld a, [wBoardGenY]
+    cp BOARD_HEIGHT - 1
+    ret z
+    inc a
+    call Board_IncrementNeighborRow
+    ret
+
+Board_IncrementNeighborRow:
+    ld d, a
+    ld a, [wBoardGenX]
+    and a
+    jr z, .center
+    dec a
+    ld e, a
+    push de
+    call Board_IncrementCellAtXY
+    pop de
+.center:
+    ld a, [wBoardGenX]
+    ld e, a
+    push de
+    call Board_IncrementCellAtXY
+    pop de
+    ld a, [wBoardGenX]
+    cp BOARD_WIDTH - 1
+    ret z
+    inc a
+    ld e, a
+    jp Board_IncrementCellAtXY
+
+Board_IncrementSideNeighbors:
+    ld d, a
+    ld a, [wBoardGenX]
+    and a
+    jr z, .right
+    dec a
+    ld e, a
+    push de
+    call Board_IncrementCellAtXY
+    pop de
+.right:
+    ld a, [wBoardGenX]
+    cp BOARD_WIDTH - 1
+    ret z
+    inc a
+    ld e, a
+    jp Board_IncrementCellAtXY
+
+; Increments the low-nibble number at (E, D) unless that cell is a mine.
+; Clobbers: AF, BC, HL
+Board_IncrementCellAtXY:
+    ld a, d
+    ld b, a
+    add a
+    add a
+    add a
+    add b
+    add e
+    ld c, a
+    ld b, 0
+    ld hl, wBoard
+    add hl, bc
+    bit CELL_MINE_BIT, [hl]
+    ret nz
+    inc [hl]
     ret
 
 ; Returns the current cursor cell index in A.
@@ -109,32 +238,52 @@ Board_RandomCellIndex:
     ret
 
 IF DEBUG_SHOW_MINES
-; Debug-only: reveal placed mines on the BG map for placement verification.
-; Called from the next VBlank after placement so VRAM writes are reliable.
-; Non-mine cells are left untouched.
-Board_DebugDrawMines:
+; Debug-only: reveal one row per VBlank so VRAM writes stay bounded.
+Board_DebugDrawNextRow:
+    ld a, [wDebugDrawRow]
+    ld b, a
+    add a
+    add a
+    add a
+    add b
+    ld c, a
+    ld b, 0
     ld hl, wBoard
+    add hl, bc
+
     ld de, BG_MAP + BOARD_BG_Y * BG_MAP_WIDTH + BOARD_BG_X
-    ld b, BOARD_HEIGHT
-.row:
+    ld a, [wDebugDrawRow]
+    ld b, a
+    and a
+    jr z, .draw
+.advanceBgRow:
+    ld a, e
+    add BG_MAP_WIDTH
+    ld e, a
+    jr nc, .nextBgRow
+    inc d
+.nextBgRow:
+    dec b
+    jr nz, .advanceBgRow
+.draw:
     ld c, BOARD_WIDTH
 .column:
+    ld a, [hl]
     bit CELL_MINE_BIT, [hl]
-    jr z, .nextCell
+    jr z, .drawNumber
     ld a, TILE_MINE
+    jr .store
+.drawNumber:
+    and $0F
+    add TILE_OPEN_0
+.store:
     ld [de], a
-.nextCell:
     inc hl
     inc de
     dec c
     jr nz, .column
-    ld a, BG_MAP_WIDTH - BOARD_WIDTH
-    add e
-    ld e, a
-    jr nc, .nextRow
-    inc d
-.nextRow:
-    dec b
-    jr nz, .row
+    ld a, [wDebugDrawRow]
+    inc a
+    ld [wDebugDrawRow], a
     ret
 ENDC
